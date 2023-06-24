45517f1ccc59a6ccd883fa5958765ed17e96c3b1
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-24 23:21:43 +0600

    Добавлены в класс Channel следующие методы: класс-метод get_service(), возвращающий объект для работы с YouTube API, метод to_json(), сохраняющий в файл значения атрибутов экземпляра Channel

--- src/channel.py
+++ src/channel.py
@@ -5,20 +5,38 @@ import isodate
 
 api_key: str = os.getenv('API_KEY_YouTube')
 
-# специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey=api_key)
-
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = 'https://www.youtube.com/channel/'+self.channel["items"][0]["id"]
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        # print(channel)
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        # api_key: str = os.getenv('API_KEY_YouTube')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename):
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        data = self.__dict__
+        del data['channel']
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
