ed16d0b2513284b51c695d215329fb9a1e8e44d8
Natalia Melnik <nataliamelnik138@gmail.com>
2023-07-04 22:05:17 +0600

    Создайн файл src/video и в нем классы Video и PLVideo

--- src/video.py
+++ src/video.py
@@ -1,7 +1,6 @@
-import json
 import os
 from googleapiclient.discovery import build
-import isodate
+
 
 api_key: str = os.getenv('API_KEY_YouTube')
 youtube = build('youtube', 'v3', developerKey=api_key)
