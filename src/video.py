ed33bdd68b223993ce9db9e90faec97968e9acae
Natalia Melnik <nataliamelnik138@gmail.com>
2023-07-11 20:53:10 +0600

    Внедрены в код блоки try/except, чтобы обработать ситуацию, когда при инициализации экземпляра класса Video пользователь передаль несуществующий id видео

--- src/video.py
+++ src/video.py
@@ -4,12 +4,18 @@ from src.channel import Channel
 class Video(Channel):
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                               id=video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v='+self.video_id
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.get_service().videos().\
+                list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            self.video_title = self.video_response['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_title = None
+            self.url = None
+            self.view_count = None
+            self.likes_count = None
 
     def __str__(self):
         return self.video_title
